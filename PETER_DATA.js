3462dbe951f515be9a0017d0957235fc63961624
Mikhail <misha2001x@gmail.com>
2023-02-27 19:35:41 +0100

    Added delivery

---
+++ PETER_DATA.js
@@ -0,0 +1,65 @@
+var PETER_DATA = {
+	type: 'FeatureCollection',
+	metadata: { name: 'СПБ', creator: 'Yandex Map Constructor' },
+	features: [
+		{
+			type: 'Feature',
+			id: 0,
+			geometry: {
+				type: 'Polygon',
+				coordinates: [
+					[
+						[30.31701729101552, 59.8982745283837],
+						[30.47906563085926, 59.891374573590824],
+						[30.5477301816405, 59.96650653135656],
+						[30.43100044531239, 60.02910069937223],
+						[30.27581856054676, 60.00572777149045],
+						[30.22363350195302, 59.939644068330836],
+						[30.31701729101552, 59.8982745283837],
+					],
+				],
+			},
+			properties: {
+				description: '140',
+				fill: '#ed4543',
+				'fill-opacity': 0.6,
+				stroke: '#ed4543',
+				'stroke-width': '5',
+				'stroke-opacity': 0.9,
+			},
+		},
+		{
+			type: 'Feature',
+			id: 1,
+			geometry: {
+				type: 'Polygon',
+				coordinates: [
+					[
+						[30.645233843749892, 60.00710310858551],
+						[30.639740679687378, 59.897584597549944],
+						[30.907532427734274, 59.867213405371984],
+						[30.952851031249875, 60.040093987504854],
+						[30.645233843749892, 60.00710310858551],
+					],
+				],
+			},
+			properties: {
+				description: '400',
+				fill: '#ed4543',
+				'fill-opacity': 0.6,
+				stroke: '#ed4543',
+				'stroke-width': '5',
+				'stroke-opacity': 0.9,
+			},
+		},
+		{
+			type: 'Feature',
+			id: 2,
+			geometry: { coordinates: [30.315644, 59.938955], type: 'Point' },
+			properties: {
+				description: 'Санкт-Петербург',
+				'marker-color': '#1e98ff',
+			},
+		},
+	],
+};
---
+++ main.js
@@ -0,0 +1,225 @@
+let GEO_DATA;
+
+$(() => {
+	GEO_DATA = KRASNODAR_DATA;
+	$('.btn-group').on('change', ({ target }) => {
+		GEO_DATA = window[target.value];
+	});
+});
+
+const init = (ym) => {
+	const suggestView = new ym.SuggestView('suggest');
+	const map = new ym.Map('map', {
+		center: [30.264981955459618, 59.9567962610097],
+		zoom: 9,
+	});
+
+	$('#button').bind('click', function (e) {
+		geocode(ym);
+	});
+
+	const geocode = async (ym) => {
+		let deliveryArea;
+		// Забираем запрос из поля ввода.
+		const request = $('#suggest').val();
+		let error;
+		let price;
+		// Геокодируем введённые данные.
+		const resultGeo = await ym.geocode(request);
+
+		const geoObject = resultGeo.geoObjects.get(0);
+
+		if (geoObject) {
+			const addressType = geoObject.properties.get(
+				'metaDataProperty.GeocoderMetaData.precision'
+			);
+			switch (addressType) {
+				case 'exact':
+					const deliveryAreas = ym.geoQuery(GEO_DATA).addToMap(map);
+					deliveryArea = deliveryAreas
+						.searchContaining(geoObject)
+						.get(0);
+					break;
+				case 'number':
+				case 'near':
+				case 'range':
+					error = 'Неточный адрес, требуется уточнение';
+					break;
+				case 'street':
+					error = 'Неполный адрес, требуется уточнение';
+					break;
+				case 'other':
+				default:
+					error = 'Неточный адрес, требуется уточнение';
+					break;
+			}
+
+			if (deliveryArea) {
+				price = deliveryArea.properties.get('description');
+			} else if (!error) {
+				error = 'По данному адресу не доставляем';
+			}
+		} else {
+			error = 'Адрес не найден';
+		}
+
+		if (error) {
+			$('#info').css({ display: 'none' });
+			$('#error').css({ display: 'block' });
+			$('#error').text(error);
+		} else {
+			$('#info').css({ display: 'block' });
+			$('#info').text(`Стоимость доставки: ${price}`);
+			$('#error').css({ display: 'none' });
+		}
+	};
+};
+
+ymaps.ready(init);
+
+// function init() {
+// 	// Подключаем поисковые подсказки к полю ввода.
+// 	var suggestView = new ymaps.SuggestView('suggest'),
+// 		map = new ymaps.Map('map', {
+// 			center: [55.7, 37.6],
+// 			zoom: 10,
+// 		}),
+// 		placemark;
+
+// 	// При клике по кнопке запускаем верификацию введёных данных.
+// 	$('#button').bind('click', function (e) {
+// 		geocode();
+// 	});
+
+// 	function geocode() {
+// 		// Забираем запрос из поля ввода.
+// 		var request = $('#suggest').val();
+// 		// Геокодируем введённые данные.
+// 		ymaps.geocode(request).then(
+// 			function (res) {
+// 				var obj = res.geoObjects.get(0),
+// 					error,
+// 					hint;
+
+// 				try {
+// 					if (obj) {
+// 						console.log('FF');
+// 						const a = ymaps.geoQuery(GEO_DATA).addToMap(map);
+
+// 						const b = a.searchContaining(obj);
+// 						console.log(b);
+
+// 						// Об оценке точности ответа геокодера можно прочитать тут: https://tech.yandex.ru/maps/doc/geocoder/desc/reference/precision-docpage/
+// 						switch (
+// 							obj.properties.get(
+// 								'metaDataProperty.GeocoderMetaData.precision'
+// 							)
+// 						) {
+// 							case 'exact':
+// 								break;
+// 							case 'number':
+// 							case 'near':
+// 							case 'range':
+// 								error = 'Неточный адрес, требуется уточнение';
+// 								hint = 'Уточните номер дома';
+// 								break;
+// 							case 'street':
+// 								error = 'Неполный адрес, требуется уточнение';
+// 								hint = 'Уточните номер дома';
+// 								break;
+// 							case 'other':
+// 							default:
+// 								error = 'Неточный адрес, требуется уточнение';
+// 								hint = 'Уточните адрес';
+// 						}
+// 					} else {
+// 						error = 'Адрес не найден';
+// 						hint = 'Уточните адрес';
+// 					}
+
+// 					// Если геокодер возвращает пустой массив или неточный результат, то показываем ошибку.
+// 					if (error) {
+// 						showError(error);
+// 						showMessage(hint);
+// 					} else {
+// 						showResult(obj);
+// 					}
+// 				} catch (e) {
+// 					console.log(e);
+// 				}
+// 			},
+// 			function (e) {
+// 				console.log(e);
+// 			}
+// 		);
+// 	}
+// 	function showResult(obj) {
+// 		// Удаляем сообщение об ошибке, если найденный адрес совпадает с поисковым запросом.
+// 		$('#suggest').removeClass('input_error');
+// 		$('#notice').css('display', 'none');
+
+// 		var mapContainer = $('#map'),
+// 			bounds = obj.properties.get('boundedBy'),
+// 			// Рассчитываем видимую область для текущего положения пользователя.
+// 			mapState = ymaps.util.bounds.getCenterAndZoom(bounds, [
+// 				mapContainer.width(),
+// 				mapContainer.height(),
+// 			]),
+// 			// Сохраняем полный адрес для сообщения под картой.
+// 			address = [obj.getCountry(), obj.getAddressLine()].join(', '),
+// 			// Сохраняем укороченный адрес для подписи метки.
+// 			shortAddress = [
+// 				obj.getThoroughfare(),
+// 				obj.getPremiseNumber(),
+// 				obj.getPremise(),
+// 			].join(' ');
+// 		// Убираем контролы с карты.
+// 		mapState.controls = [];
+// 		// Создаём карту.
+// 		createMap(mapState, shortAddress);
+// 		// Выводим сообщение под картой.
+// 		showMessage(address);
+// 	}
+
+// 	function showError(message) {
+// 		$('#notice').text(message);
+// 		$('#suggest').addClass('input_error');
+// 		$('#notice').css('display', 'block');
+// 		// Удаляем карту.
+// 		// if (map) {
+// 		// 	map.destroy();
+// 		// 	map = null;
+// 		// }
+// 	}
+
+// 	function createMap(state, caption) {
+// 		// Если карта еще не была создана, то создадим ее и добавим метку с адресом.
+// 		if (!map) {
+// 			map = new ymaps.Map('map', state);
+// 			placemark = new ymaps.Placemark(
+// 				map.getCenter(),
+// 				{
+// 					iconCaption: caption,
+// 					balloonContent: caption,
+// 				},
+// 				{
+// 					preset: 'islands#redDotIconWithCaption',
+// 				}
+// 			);
+// 			map.geoObjects.add(placemark);
+// 			// Если карта есть, то выставляем новый центр карты и меняем данные и позицию метки в соответствии с найденным адресом.
+// 		} else {
+// 			map.setCenter(state.center, state.zoom);
+// 			placemark.geometry.setCoordinates(state.center);
+// 			placemark.properties.set({
+// 				iconCaption: caption,
+// 				balloonContent: caption,
+// 			});
+// 		}
+// 	}
+
+// 	function showMessage(message) {
+// 		$('#messageHeader').text('Данные получены:');
+// 		$('#message').text(message);
+// 	}
+// }
